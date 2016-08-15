53d419459fa8b496d3d4a32ea6d8bf15c880ba2d
HUSSAR-mtrela <martin.trela@gmail.com>
2016-08-14 22:29:20 -0400

    porting org dds files for eco board

---
+++ textures/ui/cybran/game/resource-bars/resource-bars_layout.lua
@@ -0,0 +1,6 @@
+layout = {
+	['mini-energy-bar_bmp'] = {left = 0, top = 0, width = 160, height = 16, leftOffset = 0, topOffset = 0, },
+	['mini-energy-bar-back_bmp'] = {left = 0, top = 0, width = 160, height = 16, leftOffset = 0, topOffset = 0, },
+	['mini-mass-bar_bmp'] = {left = 0, top = 0, width = 160, height = 16, leftOffset = 0, topOffset = 1, },
+	['mini-mass-bar-back_bmp'] = {left = 0, top = 0, width = 160, height = 16, leftOffset = 0, topOffset = 0, },
+}
